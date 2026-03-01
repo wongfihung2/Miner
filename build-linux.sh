dd5730ee41100c3cf622b0cc03163596b1ac1f3c
wong-fi-hung <wong-fi-hung@github.com>
2026-03-01 14:20:55 +0700

    Update build-linux.sh

--- build-linux.sh
+++ build-linux.sh
@@ -3,19 +3,19 @@
 # Default for cortex-a53 optimized builds
 clear
 # -fprofile-reproducible=[serial|parallel-runs|multithreaded]
-export feature="simd+crypto+crc"
+export feature="native"
 export CC=gcc
 export CXX=g++
-export CFS="-Ofast -march=armv8-a+$feature -mcpu=cortex-a53+$feature -mtune=cortex-a53 -fvisibility=hidden -funroll-loops -fomit-frame-pointer -fprofile -fprofile-generate=./profile -fprofile-dir=./profile -fprofile-correction -fprofile-abs-path -fprofile-reorder-functions -fassociative-math -funsafe-math-optimizations -funsafe-loop-optimizations -faggressive-loop-optimizations -floop-parallelize-all -ftree-parallelize-loops=4 -ftree-loop-optimize -floop-nest-optimize -foptimize-crc -foptimize-sibling-calls -foptimize-strlen -fexpensive-optimizations -Wno-implicit-function-declaration -Wno-incompatible-pointer-types -Wno-int-conversion -Wno-error -fopenmp"
-export CFLAGS="$CFS -std=gnu23"
+export CFS="-Ofast -march=$feature -mcpu=$feature -mtune=$feature -flto -pg -maes -mavx2 -msse2 -fvisibility=hidden -funroll-loops -fomit-frame-pointer -fassociative-math -funsafe-math-optimizations -funsafe-loop-optimizations -faggressive-loop-optimizations -ftree-parallelize-loops=4 -ftree-loop-optimize -floop-nest-optimize -foptimize-sibling-calls -foptimize-strlen -fexpensive-optimizations -freorder-functions -finline-functions -finline-stringops=memcmp -finline-stringops=memcpy -finline-stringops=memmove -finline-stringops=memset -Wno-implicit-function-declaration -Wno-incompatible-pointer-types -Wno-int-conversion -Wno-error"
+export CFLAGS="$CFS -std=gnu99"
 export CXXFLAGS="$CFS -std=gnu++23"
 export LIBS="-lcurl -lcrypto -lssl"
-export LDFLAGS="-static -Wl,-s"
+export LDFLAGS="-Wl,-s"
 
 make clean
 make distclean
-#rm configure config.status
-#autoreconf -i
-./configure --with-crypto --with-curl --disable-shared CC="$CC" CXX="$CXX" CFLAGS="$CFLAGS" CXXFLAGS="$CFLAGS" LDFLAGS="$CXXFLAGS -std=gnu23 $LDFLAGS" LIBS="$LIBS"
+rm configure config.status
+autoreconf -i
+./configure --with-crypto --with-curl --disable-shared CC="$CC" CXX="$CXX" CFLAGS="$CFLAGS" CXXFLAGS="$CFLAGS" LDFLAGS="$CXXFLAGS $LDFLAGS" LIBS="$LIBS"
 nano Makefile
 make -j4
