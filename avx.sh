2f174a97934f04cbc6b3af76f0b88c553fd6fc06
wong-fi-hung <wong-fi-hung@github.com>
2026-03-18 13:38:07 +0700

    update build sc

---
+++ avx.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Default for cortex-a53 optimized builds
+clear
+
+export feature="native"
+export CC=gcc
+export CXX=g++
+export CFS="-O3 -Ofast -march=$feature -mcpu=$feature -mtune=$feature -maes -mavx2 -D__AVX2__ -DUSE_ASM -pg -fvisibility=hidden -funroll-loops -fomit-frame-pointer -finline-functions -Wno-implicit-function-declaration -Wno-incompatible-pointer-types -Wno-int-conversion -Wno-error -std=gnu99"
+export CXS="-O3 -Ofast -march=$feature -mcpu=$feature -mtune=$feature -maes -mavx2 -D__AVX2__ -DUSE_ASM -pg -fvisibility=hidden -funroll-loops -fomit-frame-pointer -finline-functions -Wno-implicit-function-declaration -Wno-incompatible-pointer-types -Wno-int-conversion -Wno-error -std=gnu++17"
+export CFLAGS="$CFS"
+export CXXFLAGS="$CXS"
+export LIBS="-lcurl -lcrypto -lssl"
+export LDFLAGS="-Wl,-s"
+
+make clean
+make distclean
+rm configure config.status
+autoreconf -i
+./configure --with-crypto --with-curl --disable-shared CC="$CC" CXX="$CXX" CFLAGS="$CFLAGS" CXXFLAGS="$CFLAGS" LDFLAGS="$CXXFLAGS $LDFLAGS" LIBS="$LIBS"
+nano Makefile
+make -j4
